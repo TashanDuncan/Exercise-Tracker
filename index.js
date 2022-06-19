9d3b7872e3b6d5d03520fcd4467fd8869d6a6520
Tashan Duncan <tashanduncan1994@gmail.com>
2022-06-19 07:05:57 +0100

    create user post and get requests

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mySecret = process.env['MONGO_URI'];
+const User = require('./models/user');
 
 
 mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,6 +22,24 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.route('/api/users').get((req,res) =>{
+  User.find({},(err, users) =>{
+    if (err) return console.error(err);
+    res.json(users);
+    console.log('retrieved list of names', users.length, users[0].name);
+})
+}).post((req,res) =>{
+  const {username} = req.body
+
+  let newuser = new User({
+    username
+  });
+  newuser.save((err, data) => {
+    if (err) return console.error(err);
+    return res.json(data)
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
